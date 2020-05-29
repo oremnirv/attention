df313c390609ea8749e523a53cea6fc7cefb4c71
Omer Nivron <omernivron@Omers-MacBook-Pro.local>
2020-05-29 13:59:19 -0400

    added gp prior scripts

---
+++ gp_kernels.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def rbf_kernel(x, sigma=1):
+    nrow, ncol = np.shape(x)
+    k = np.matmul(x, np.transpose(x)) / (sigma ** 2)
+    d = np.diag(k)
+    # subtract diagonal one time as a row and once as a column
+    k1 = k - (d.reshape(-1, 1) / (2 * (sigma ** 2)))
+    k2 = k1 - (d.reshape(1, -1) / (2 * (sigma ** 2)))
+    k3 = np.exp(k2)
+    return k3
+
+
+def rbf_n_white_kernel(ɾ, λ, σ):
+    return (ɾ**2 * gp.kernels.RBF(length_scale=λ) + gp.kernels.WhiteKernel(noise_level=σ))
