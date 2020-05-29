4892daf48a3e019bfcfa97eba1fbd03bf610f3b8
Omer Nivron <omernivron@Omers-MacBook-Pro.local>
2020-05-29 14:28:42 -0400

    indentation correction in gp_priors

--- gp_priors.py
+++ gp_priors.py
@@ -2,5 +2,5 @@ import numpy as np
 
 def generate_priors(kernel, num_points, num_func):
 	L = np.linalg.cholesky(k + np.exp(-6) * np.eye(n))
-    f_prior = np.dot(L, np.random.normal(size=(n, num_func)))
+	f_prior = np.dot(L, np.random.normal(size=(n, num_func)))
     return f_prior.reshape(num_func, -1)
