b97cd67b2cfb60a03015acad6fa52eb6a6260c14
Omer Nivron <omernivr12+git@gmail.com>
2020-07-04 19:54:07 +0200

    added normalization and droput layers

--- model/model.py
+++ model/model.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 
 class Decoder(tf.keras.layers.Layer):
-    def __init__(self, l):
+    def __init__(self, l, rate=0.1):
         super(Decoder, self).__init__()
         
         self.l = l
@@ -20,6 +20,14 @@ class Decoder(tf.keras.layers.Layer):
         
         self.B = tf.keras.layers.Dense(l, name = 'B')
         self.A = tf.keras.layers.Dense(1, name = 'A')
+
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+    
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+        self.dropout3 = tf.keras.layers.Dropout(rate)
+
 
     #a call method, the layer's forward pass
     def call(self, tar_position, tar_inp, training, pos_mask, tar_mask):
@@ -37,6 +45,8 @@ class Decoder(tf.keras.layers.Layer):
         #shape=(128, 59, 16)
         
         pos_attn1 = dot_prod_attention.dot_prod_position(q_p, k_p, v_p, mask = pos_mask)
+        pos_attn1 = self.dropout1(pos_attn1, training = training)
+        pos_attn1 = self.layernorm1(pos_attn1)
 #         print('pos_attn1 :', pos_attn1)
 #       shape=(128, 58, 16, 16)
     
@@ -51,6 +61,8 @@ class Decoder(tf.keras.layers.Layer):
 #       shape=(128, 58, 16)
 
         tar_attn1, _, _ = dot_prod_attention.dot_product_attention(q, k, v, tar_mask)
+        tar_attn1 = self.dropout2(tar_attn1, training = training)
+        tar_attn1 = self.layernorm2(tar_attn1)
         # tar_attn1 is (batch_size, max_seq_len - 1, tar_d_model)
 
 #         print('tar_attn1 :', tar_attn1)
@@ -64,6 +76,7 @@ class Decoder(tf.keras.layers.Layer):
         # shape=(128, 58, 16, 16)
 
         L = tf.matmul(tar1, tf.cast(pos_attn1, tf.float64))
+        L = self.dropout3(L, training = training) 
         
 #         print('L :', L)
         # shape=(128, 58, 16, 16)
