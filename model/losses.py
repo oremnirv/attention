76136d1916223113e2094b5f4b451f073efdc7b6
Omer Nivron <omernivr12+git@gmail.com>
2020-07-09 11:51:36 +0200

    adding some print statements to try and identify problem with test loss

--- model/losses.py
+++ model/losses.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 loss_object = tf.keras.losses.MeanSquaredError()
 
-def loss_function(real, pred, pred_log_sig, epsilon = 0.0001):
+def loss_function(real, pred, pred_log_sig, epsilon = 0.001):
     '''
     Masked MSE. Since the target sequences are padded, 
     it is important to apply a padding mask when calculating the loss.
@@ -18,6 +18,13 @@ def loss_function(real, pred, pred_log_sig, epsilon = 0.0001):
     # tf.print(pred_log_sig)
     mask = tf.math.logical_not(tf.math.equal(real, 0))
     mse = loss_object(real, pred)
+    print('mse: ')
+    tf.print(mse)
+    print('mse divided: ')
+    tf.print(tf.math.divide(mse, tf.math.square(tf.math.exp(pred_log_sig)) + epsilon))
+    print('pred_log_sig: ')
+    tf.print(pred_log_sig)
+
     loss_ =  (tf.math.divide(mse, tf.math.square(tf.math.exp(pred_log_sig)) + epsilon) + (pred_log_sig))
 
     
