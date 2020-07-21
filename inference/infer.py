68a4ad602a61c2569446c813c35551017aaca32d
Omer Nivron <omernivr12+git@gmail.com>
2020-07-21 11:22:24 +0200

    adpated infer py to include model and pred_log_sigma

--- inference/infer.py
+++ inference/infer.py
@@ -12,16 +12,17 @@ def evaluate(model, pos, tar, pos_mask):
     pos_mask: position mask tensor to hide unseen positions from current prediction 
     ------------------
     Returns:
-    out (tf tensor float64): the prediction of the next location in the sequence 
+    pred (tf tensor float64): the prediction of the next location in the sequence 
+    pred_log_sig (tf tensor float64)
     
     '''
     combined_mask_tar = create_masks(tar)
-    out = model(pos, tar, False, pos_mask, combined_mask_tar)
-    return out
+    pred, pred_log_sig = model(pos, tar, False, pos_mask, combined_mask_tar)
+    return pred, pred_log_sig 
 
 
 
-def inference(pos, tar, num_steps = 1):
+def inference(model, pos, tar, num_steps = 1):
     '''
     how many steps to infer -- this could be used both for interpolation and extrapolation 
     ------------------
@@ -31,18 +32,17 @@ def inference(pos, tar, num_steps = 1):
     num_steps (int): how many inference steps are required
     ------------------
     Returns:
-    out (tf.tensor float64): the predictions for all timestamps up to n + num_steps  
-    
+    pred (tf.tensor float64): the predictions for all timestamps up to n + num_steps  
+    pred_log_sig
     '''
     n = tar.shape[1]
     temp_pos = pos[:, :(n + 1)]
     pos_mask = position_mask(temp_pos)
     
-    out = evaluate(temp_pos, tar, pos_mask)
-#     print(out[n - 1])
-    tar = tf.concat((tar, tf.reshape(out[n - 1], [1, 1])), axis = 1)
+    pred, pred_log_sig = evaluate(model, temp_pos, tar, pos_mask)
+    tar = tf.concat((tar, tf.reshape(pred[n - 1], [1, 1])), axis = 1)
     if num_steps > 1:
-        out = inference(pos, tar, num_steps - 1)
+        pred, pred_log_sig  = inference(pos, tar, num_steps - 1)
     
-    return out
+    return pred, pred_log_sig 
     
