99e2a747a460cf44794b4fd004adaecdd1e18236
Omer Nivron <omernivr12+git@gmail.com>
2020-07-22 19:43:56 +0200

    working river notebook still without time2 feature and 27 attributes

--- data/batch_creator.py
+++ data/batch_creator.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 
+
 def create_batch_gp_mim(enc_tr, dec_tr, y_tr, batch_s=128):
     '''
     This is a batch for training attention models. The idea
@@ -31,6 +32,7 @@ def create_batch_gp_mim(enc_tr, dec_tr, y_tr, batch_s=128):
     batch_y_tr = (y_tr[batch_idx_tr].reshape(-1, 1))
     return batch_enc_tr, batch_dec_tr, batch_y_tr, batch_idx_tr
 
+
 def create_batch_gp_mim_2(pos, tar, batch_s=128):
     '''
     Get a batch of positions, targets and position mask from data generated 
@@ -47,7 +49,7 @@ def create_batch_gp_mim_2(pos, tar, batch_s=128):
     batch_tar_tr (2D np array)
     batch_pos_mask (4D np array)
     batch_idx_tr (1D np array): indices (=row numbers) chosen for current batch
-    
+
     '''
     shape = tar.shape[0]
     batch_idx_tr = np.random.choice(list(range(shape)), batch_s)
@@ -59,7 +61,7 @@ def create_batch_gp_mim_2(pos, tar, batch_s=128):
 def create_batch_foxes(token_pos, time_pos, tar, batch_s=128):
     '''
 
-    
+
     '''
     shape = tar.shape[0]
     batch_idx_tr = np.random.choice(list(range(shape)), batch_s)
@@ -67,3 +69,23 @@ def create_batch_foxes(token_pos, time_pos, tar, batch_s=128):
     batch_tok_pos_tr = token_pos[batch_idx_tr, :]
     batch_tim_pos_tr = time_pos[batch_idx_tr, :]
     return batch_tok_pos_tr, batch_tim_pos_tr, batch_tar_tr, batch_idx_tr
+
+
+def create_batch_river(token_pos, time_pos1, time_pos2, x, att, tar, batch_s=128):
+    '''
+    '''
+    shape = tar.shape[0]
+    batch_idx_tr = np.random.choice(list(range(shape)), batch_s)
+    batch_tar_tr = tar[batch_idx_tr, :]
+    batch_tok_pos_tr = token_pos[batch_idx_tr, :]
+    batch_tim_pos_tr = time_pos1[batch_idx_tr, :]
+    batch_tim_pos_tr2 = time_pos2[batch_idx_tr, :]
+    xx = x[batch_idx_tr, :]
+    batch_pos_tr = np.zeros((batch_s, att.shape[1] - 1, tar.shape[1]))
+    for i in range(xx.shape[0]):
+        batch_pos_tr[i, :, :] = np.concatenate(((np.repeat(np.array(att.iloc[np.where
+                                                                ([att['gauge_id'] == xx[i, 0]])[1][0], 1:]).reshape(1, -1), 25)).reshape(-1, 25), (np.repeat(np.array(att.iloc[np.where
+                                                                                                                                                                                                            ([att['gauge_id'] == xx[i, -1]])[1][0], 1:]).reshape(1, -1), 25)).reshape(-1, 25)), axis=1)
+
+
+    return batch_tok_pos_tr, batch_tim_pos_tr, batch_tim_pos_tr2, batch_pos_tr, batch_tar_tr, batch_idx_tr
