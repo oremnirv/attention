8385da3e38d19a3ebe0a319c1373285914729e34
Omer Nivron <omernivr12+git@gmail.com>
2020-12-12 16:54:39 +0000

    fixing inference to enable sampling

--- model/classic_model.py
+++ model/classic_model.py
@@ -30,7 +30,7 @@ class Decoder(tf.keras.layers.Layer):
         self.A4 = tf.keras.layers.Dense(2, name = 'A4')
 
 
-        self.Bsig = tf.keras.layers.Dense(32, name = 'Bsig')
+        # self.Bsig = tf.keras.layers.Dense(32, name = 'Bsig')
         self.Asig = tf.keras.layers.Dense(32, name = 'Asig')
 
 
@@ -45,12 +45,12 @@ class Decoder(tf.keras.layers.Layer):
 
 
     #a call method, the layer's forward pass
-    def call(self, tar_position, tar_inp, training, pos_mask, tar_mask):
+    def call(self, tar_position, current_pos, tar_inp, training, pos_mask, tar_mask):
         
         # Adding extra dimension to allow multiplication of 
         # a sequnce with itself. 
         # print('tar_position shape: ', tar_position.shape)
-
+        
         tar_position = tar_position[:, :, tf.newaxis]
         
         q_p = self.wq(tar_position) 
@@ -63,7 +63,7 @@ class Decoder(tf.keras.layers.Layer):
         #shape=(128, 59, 16)
         
         pos_attn1, _, _ = dot_prod_attention.dot_product_attention(q_p, k_p, v_p, pos_mask)
-        pos_attn1 =   tf.transpose(self.Asig(tf.transpose(pos_attn1,  perm = [0, 2, 1])), perm = [0, 2, 1])
+        # pos_attn1 =   tf.transpose(self.Asig(tf.transpose(pos_attn1,  perm = [0, 2, 1])), perm = [0, 2, 1])
         # pos_attn1 = self.dropout1(pos_attn1, training = training)
         # pos_attn1 = self.layernorm1(pos_attn1)
         print('pos_attn1 :', pos_attn1)
@@ -82,7 +82,7 @@ class Decoder(tf.keras.layers.Layer):
         print('tar mask :', tar_mask)
 
         tar_attn1, _, _ = dot_prod_attention.dot_product_attention(q, k, v, tar_mask)
-        tar_attn1 =   tf.transpose(self.Bsig(tf.transpose(tar_attn1,  perm = [0, 2, 1])), perm = [0, 2, 1])
+        # tar_attn1 =   tf.transpose(self.Bsig(tf.transpose(tar_attn1,  perm = [0, 2, 1])), perm = [0, 2, 1])
         # tar_attn1 = self.dropout2(tar_attn1, training = training)
         # tar_attn1 = self.layernorm2(tar_attn1)
         # tar_attn1 is (batch_size, max_seq_len - 1, tar_d_model)
@@ -110,7 +110,7 @@ class Decoder(tf.keras.layers.Layer):
         # L2 = self.A(tf.reshape(L, shape = [tf.shape(L)[0], tf.shape(L)[1] ,self.l ** 2])) 
         L2 = tf.nn.leaky_relu(self.A1(L))
         # print('L2 after A1', L2) 
-        L2 = self.A2(L2) 
+        L2 = self.A2(L2) + self.Asig(current_pos[:, :, tf.newaxis]) 
         L2 = tf.nn.leaky_relu(self.A3(L2)) 
         # print('L2', L2)
         L2 = self.A4(L2)
