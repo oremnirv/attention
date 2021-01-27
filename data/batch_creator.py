9297774784e13f0b00c076ac77fad62baa69ba2b
oremnirv <xyfqap-9tydni-voZpig>
2021-01-27 12:00:52 +0000

    changed names of pos and tar to x and y in unn notebook

--- data/batch_creator.py
+++ data/batch_creator.py
@@ -1,31 +1,31 @@
 import numpy as np
 
 
-def create_batch(em_pos, pos, tar, batch_s=128, chnge_context=True, d=False, em_2=None, time = False, context_p = 50):
+def create_batch(em_x, x, y, batch_s=128, chnge_context=True, d=False, em_2=None, time = False, context_p = 50):
     """
-    Get a batch of positions, targets and position mask from data generated
-    by data_generator_for_gp_mimick_gpt function and from position_mask function
+    Get a batch of xitions, ygets and xition mask from data generated
+    by data_generator_for_gp_mimick_gpt function and from xition_mask function
     -------------------------
     Parameters:
-    pos (2D np array): 1st/2nd output from data_generator_for_gp_mimick_gpt function
-    tar (2D np array): 3rd/4th output from data_generator_for_gp_mimick_gpt function
+    x (2D np array): 1st/2nd output from data_generator_for_gp_mimick_gpt function
+    y (2D np array): 3rd/4th output from data_generator_for_gp_mimick_gpt function
     batch_s (int): deafult 128
     -------------------------
     Returns:
-    batch_pos_tr (2D np array)
-    batch_tar_tr (2D np array)
+    batch_x_tr (2D np array)
+    batch_y_tr (2D np array)
     batch_idx (1D np array): indices (=row numbers) chosen for current batch
 
     """
     b_data = []
-    shape = tar.shape[0]
-    cols = tar.shape[1]
+    shape = y.shape[0]
+    cols = y.shape[1]
     batch_idx = np.random.choice(list(range(shape)), batch_s)
 
     if not chnge_context:
-        b_data.append(tar[batch_idx])
-        b_data.append(pos[batch_idx])
-        b_data.append(em_pos[batch_idx])
+        b_data.append(y[batch_idx])
+        b_data.append(x[batch_idx])
+        b_data.append(em_x[batch_idx])
         if d:
             b_data.append(em_2[batch_idx])
     else:
@@ -36,32 +36,32 @@ def create_batch(em_pos, pos, tar, batch_s=128, chnge_context=True, d=False, em_
 
         else:
             permute_idx = np.random.permutation(np.arange(cols))
-        b_data.append(tar[batch_idx][:, permute_idx])
-        b_data.append(pos[batch_idx][:, permute_idx])
-        b_data.append(em_pos[batch_idx][:, permute_idx])
+        b_data.append(y[batch_idx][:, permute_idx])
+        b_data.append(x[batch_idx][:, permute_idx])
+        b_data.append(em_x[batch_idx][:, permute_idx])
         if d:
             b_data.append(em_2[batch_idx][:, permute_idx])
     return b_data
 
 
-def fake_batch(pos, tar, batch_s=1):
+def fake_batch(x, y, batch_s=1):
     """
-    Get a batch of positions, targets and position mask from data generated
-    by data_generator_for_gp_mimick_gpt function and from position_mask function
+    Get a batch of xitions, ygets and xition mask from data generated
+    by data_generator_for_gp_mimick_gpt function and from xition_mask function
     -------------------------
     Parameters:
-    pos (2D np array): 1st/2nd output from data_generator_for_gp_mimick_gpt function
-    tar (2D np array): 3rd/4th output from data_generator_for_gp_mimick_gpt function
+    x (2D np array): 1st/2nd output from data_generator_for_gp_mimick_gpt function
+    y (2D np array): 3rd/4th output from data_generator_for_gp_mimick_gpt function
     batch_s (int): deafult 128
     -------------------------
     Returns:
-    batch_pos_tr (2D np array)
-    batch_tar_tr (2D np array)
+    batch_x_tr (2D np array)
+    batch_y_tr (2D np array)
     batch_idx (1D np array): indices (=row numbers) chosen for current batch
 
     """
-    shape = tar.shape[0]
+    shape = y.shape[0]
     batch_idx = np.random.choice(list(range(shape)), batch_s)
-    batch_tar_tr = np.tile(tar[batch_idx], 2).reshape(2, -1)
-    batch_pos_tr = np.tile(pos[batch_idx], 2).reshape(2, -1)
-    return batch_pos_tr, batch_tar_tr, batch_idx
+    batch_y_tr = np.tile(y[batch_idx], 2).reshape(2, -1)
+    batch_x_tr = np.tile(x[batch_idx], 2).reshape(2, -1)
+    return batch_x_tr, batch_y_tr, batch_idx
