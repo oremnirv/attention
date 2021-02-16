fixed a bug of masking, ignored first row instead of last row

inference/infer.py
@@ -24,9 +24,9 @@ def evaluate(model, x, y, sample=True, d=False, x2=None, xx=None, yy=None, c_ste
     """
     combined_mask_x = masks.create_masks(x)
     if d:
-        pred = model(x, x2, y, False, combined_mask_x[:, 1:, :-1], infer=infer, ix=xx, iy=yy, n=c_step, x0=x0, y0=y0, x1=x1, y1=y1)
+        pred = model(x, x2, y, False, combined_mask_x[:, :-1, :-1], infer=infer, ix=xx, iy=yy, n=c_step, x0=x0, y0=y0, x1=x1, y1=y1)
     else:
-        pred = model(x, y, False, combined_mask_x[:, 1:, :-1])
+        pred = model(x, y, False, combined_mask_x[:, :-1, :-1])
     if sample:
         sample_y = np.random.normal(pred[-1, 0], np.exp(pred[-1, 1]))
     else:

main.py
@@ -11,7 +11,7 @@ import numpy as np
 
 kernel = input()
 d = True
-save_dir = '/home/azureuser/Downloads/GPT_' + kernel
+save_dir = os.path.expanduser('/home/azureuser/Downloads/GPT_' + kernel)
 data = loader.load_data(kernel, size = 1, rewrite = False, diff_x= True, noise = False, d = True, ordered = True)
 train_step, test_step, loss_object, train_loss, test_loss, m_tr, m_te = grapher.build_graph()
 EPOCHS = 75; batch_s  = 64; run = 515; step = 0; train_steps = 35000; heads = 32; ℯ = 512; context = 10
@@ -37,7 +37,6 @@ if manager.latest_checkpoint:
     print("Restored from {}".format(manager.latest_checkpoint))
 else:
     print("Initializing from scratch.")
-#     optimizer_c = tf.keras.optimizers.Adam(3e-4)
 with writer.as_default():
     for epoch in range(EPOCHS):
         
@@ -68,10 +67,8 @@ with writer.as_default():
                 if d:
                     pass
                     # pred_te, pred_log_te = test_step(decoder, test_loss, m_te, x_te = data[3][:500, :], y_te = data[6][:500, :], d = True, x2_te = data[0][:500, :])
-                    # plotter.follow_training_plot2d(x_tr = b_data[1], y_tr = b_data[0], em_2_tr = b_data[3] , pred = pred, x_te = data[2][:500], y_te = data[-2][:500], em_2_te = data[0][:500] ,pred_te = pred_te, num_context = context)
                 else:
                     pred_te, pred_log_te = test_step(decoder, test_loss, m_te, x_te = data[2][:500, :], y_te = data[5][:500, :], context_p = context)
-                    plotter.follow_training_plot(x_tr = b_data[1], y_tr = b_data[0], pred = pred, x_te = data[1][:500, :], y_te = data[5][:500, :], pred_te = pred_te, num_context = context)
                 helpers.print_progress(epoch, batch_n, train_loss.result(), test_loss.result(), m_tr.result(), m_te.result())
                 helpers.tf_summaries(run, step, train_loss.result(), test_loss.result(), m_tr.result(), m_te.result(), weights, names)
                 print('learning rate is {}'.format(optimizer_c._decayed_lr('float32').numpy()))

model/dot_prod_attention.py
@@ -23,18 +23,18 @@ def dot_product_attention(q, k, v, mask, infer=False, x=None, y=None, n=0, x0=No
     :return:
     """
     # q = tf.divide(q, tf.norm(q, axis=))
-    print(q.shape)
     mask = mask[:, tf.newaxis, :, :]
     matmul_qk = tf.matmul(q, k, transpose_b=True, name='qk')
     matmul_qk = matmul_qk[:, :, 1:, :-1]
     # print(matmul_qk.shape)
     dk = tf.cast(tf.shape(k)[-1], tf.float64)
-    nl_qk = tf.cast(tf.nn.relu(matmul_qk / tf.math.sqrt(dk), name='nl_qk'), tf.float64)
+    # nl_qk = tf.cast(tf.nn.relu(matmul_qk / tf.math.sqrt(dk), name='nl_qk'), tf.float64)
+    nl_qk = tf.cast(matmul_qk / tf.math.sqrt(dk), tf.float64)
     if mask is not None:
         nl_qk += ((tf.cast(mask, tf.float64)) * -1e9)
     att_weights = tf.nn.softmax(nl_qk, axis=-1, name='att_weights')  # batch_size X d_model X seq_len X seq_len
     if infer:
-        k_vals, k_ind = tf.math.top_k(att_weights[0, -1, -1, :], k=5, sorted=True, name=None)
+        k_vals, k_ind = tf.math.top_k(att_weights[0, :, -1, :], k=5, sorted=True, name=None)
         # k_vals_agg, k_ind_agg = tf.math.top_k(k_ind.reshape(-1), k=10, sorted=True, name=None)
         plt.figure(n)
         # print('x0: ', x0)

model/experimental2d_model.py
@@ -7,7 +7,7 @@ from model import dot_prod_attention
 
 
 class Decoder(tf.keras.Model):
-    def __init__(self, e, l1=512, l2=256, l3=32, num_heads=1, input_vocab_size=2000):
+    def __init__(self, e, l1=256, l2=128, l3=32, num_heads=1, input_vocab_size=2000):
         super(Decoder, self).__init__()
         self.e = e
         self.embedding = tf.keras.layers.Embedding(input_vocab_size, e)
@@ -26,7 +26,7 @@ class Decoder(tf.keras.Model):
         # print(tf.divide(x, norm))
         x_2 = self.embedding(x_2)
         y_attn, _ = self.mha(y, x, x, x_2, x_mask, infer=infer, x=ix, y=iy, n=n, x0=x0, y0=y0, x1=x1, y1=y1)
-        y_attn = tf.nn.leaky_relu(y_attn)
+        # y_attn = tf.nn.leaky_relu(y_attn)
         current_position = x[:, 1:, :]
         current_series = x_2[:, 1:, :]
         # print(current_series.shape)

model/grapher.py
@@ -36,9 +36,9 @@ def build_graph():
         combined_mask_x = masks.create_masks(xx)
         with tf.GradientTape(persistent=True) as tape:
             if d:
-                pred = decoder(x, x2, y_inp, True, combined_mask_x[:, 1:, :-1])
+                pred = decoder(x, x2, y_inp, True, combined_mask_x[:, :-1, :-1])
             else:
-                pred = decoder(x, y_inp, True, combined_mask_x[:, 1:, :-1])
+                pred = decoder(x, y_inp, True, combined_mask_x[:, :-1, :-1])
             if type(context_p) is list:
                 pred0 = tf.squeeze(pred[:, :, 0])
                 pred1 = tf.squeeze(pred[:, :, 1])