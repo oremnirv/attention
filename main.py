9381252273f8a2a3c2d8d2e5bee25780f5b2e8c4
oremnirv <xyfqap-9tydni-voZpig>
2021-02-16 15:08:34 +0000

    changing code to run on vm

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import numpy as np
 
 kernel = 'rbf_rbf_2D'
 d = True
-save_dir = '/home/omer/Downloads/GPT_' + kernel
+save_dir = '/home/azureuser/Downloads/GPT_' + kernel
 data = loader.load_data(kernel, size = 1, rewrite = False, diff_x= True, noise = False, d = True, ordered = True)
 train_step, test_step, loss_object, train_loss, test_loss, m_tr, m_te = grapher.build_graph()
 EPOCHS = 75; batch_s  = 64; run = 777; step = 0; train_steps = 35000; heads = 32; ℯ = 512; context = 10
