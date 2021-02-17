32c033296d3e3f68cff1458c00a293fd61ffee79
oremnirv <xyfqap-9tydni-voZpig>
2021-02-17 12:10:34 +0000

    start of day git

---
+++ vm_setup/setup1.sh
@@ -0,0 +1,6 @@
+yes Y | command-that-asks-for-input
+mkdir ~/Downloads
+wget https://aka.ms/downloadazcopy-v10-linux
+tar -xvf downloadazcopy-v10-linux
+cd azcopy_linux_amd64_10.8.0/
+
---
+++ vm_setup/setup2.sh
@@ -0,0 +1,15 @@
+yes Y | command-that-asks-for-input
+cp -r ~/Downloads/Downloads/GPT_rbf_const_2D ~/Downloads/
+cd 
+git clone https://github.com/oremnirv/attention.git
+cd attention
+git checkout omer
+sudo add-apt-repository universe
+sudo apt-get update
+sudo apt install python3-pip
+pip3 install --upgrade pip
+pip3 install tensorflow==2.1.0
+pip3 install matplotlib
+pip3 install sklearn
+pip3 install pandas
+pip3 install keras
