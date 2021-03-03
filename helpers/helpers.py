ac249fbd9ea12900d44514aed7296aeda8910e61
oremnirv <xyfqap-9tydni-voZpig>
2021-03-03 13:44:53 +0000

    Cleaniing code in grapher

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
+from model import experimental2d_model
+
 
 def mkdir(folder):
     """
@@ -38,6 +40,7 @@ def print_progress(epoch, batch_n, train_loss_r, test_loss_r, tr_metric, te_metr
             epoch, batch_n,
             train_loss_r, test_loss_r, tr_metric, te_metric))
 
+
 def tensorboard_embeddings(model, layer_num, meta_data, logdir):
     from tensorboard.plugins import projector
     # Save the weights we want to analyse as a variable. Note that the first
@@ -64,9 +67,10 @@ def tensorboard_embeddings(model, layer_num, meta_data, logdir):
     projector.visualize_embeddings(logdir, config)
 
 
-def write_speci(folder, names, shapes, context_p):
+def write_speci(folder, names, shapes, context_p, heads):
     with open(os.path.expanduser(folder + '_context_' + str(context_p) + '_speci.csv'), "w") as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow('heads', str(heads))
         for name, shape in zip(names, shapes):
             writer.writerow([str(name.numpy()).split('/')[-2], str(shape.numpy())])
 
@@ -93,3 +97,38 @@ def load_spec(path, e, l, context_p, d=False, old=False, abc=False):
             for i in [1, 9, 11, 13, 15]:
                 ls.append(int(df[i][1].split('[')[1].split(']')[0]))
         return ls
+
+
+def pre_trained_loader(x, save_dir, e, l, d=True, batch_s=64, context=50, heads=1, run=9999):
+    name_comp = 'run_' + str(run)
+    logdir = save_dir + '/logs/' + name_comp
+    writer = tf.summary.create_file_writer(logdir)
+    folder = save_dir + '/ckpt/check_' + name_comp
+    optimizer_c = tf.keras.optimizers.Adam(3e-4)
+    e, l1, _, l2, l3, heads = load_spec(folder, e, l, heads, context, d=d, abc=True)
+    mkdir(folder)
+    if d:
+        decoder = experimental2d_model.Decoder(e, l1, l2, l3, num_heads=heads)
+    else:
+        decoder = experimental_model.Decoder(e, l1, l2, l3, num_heads=heads)
+    num_batches = int(x.shape[0] / batch_s)
+    ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer_c, net=decoder)
+    manager = tf.train.CheckpointManager(ckpt, folder, max_to_keep=3)
+    ckpt.restore(manager.latest_checkpoint)
+    if manager.latest_checkpoint:
+        print("Restored from {}".format(manager.latest_checkpoint))
+    else:
+        print("Initializing from scratch.")
+    return decoder, ckpt, manager, num_batches, writer
+
+
+def gather_idx(c, x):
+    if type(c) is list:
+        cols = [np.arange(c[i], x.shape[1] - 1, 1) for i in range(len(c))]
+        cc = np.concatenate(cols, axis=0)
+        rows = [np.repeat(i, len(m)) for i, m in enumerate(cols)]
+        r = np.concatenate(rows, axis=0)
+        to_gather = np.concatenate((r.reshape(-1, 1), cc.reshape(-1, 1)), 1)
+    else:
+        to_gather = None
+    return to_gather
