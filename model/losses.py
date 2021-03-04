1161ed919c290f8d604e79ddc619f60a442d206b
oremnirv <xyfqap-9tydni-voZpig>
2021-03-04 14:39:21 +0000

    Cleaniing code in grapher

--- model/losses.py
+++ model/losses.py
@@ -20,7 +20,13 @@ def loss_function(real, pred, pred_log_sig=None, epsilon=0.001):
     """
     mask = tf.math.logical_not(tf.math.equal(real, 0))
     mse = tf.math.square(tf.math.subtract(real, pred))
+    print('mask: ', mask)
+    print('mse: ', mse)
     loss_ = 1 / 2 * (tf.math.divide(mse, tf.math.square(tf.math.exp(pred_log_sig)) + epsilon) + pred_log_sig)
-    loss_ = tf.math.reduce_mean(loss_, axis=0)
-    mse_ = tf.math.reduce_mean(mse, axis=0)
-    return tf.math.reduce_mean(loss_), tf.math.reduce_mean(mse_), mask
+    print('loss_: ', loss_)
+    print(loss_[1, 1])
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+    return tf.reduce_sum(loss_) / tf.reduce_sum(mask), mse, mask
+
+
