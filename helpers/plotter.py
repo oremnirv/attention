d53b9eb0917215a435e5f3956243447d91c4a846
oremnirv <xyfqap-9tydni-voZpig>
2021-03-05 16:22:06 +0000

    fixed an issue with gather in helper

--- helpers/plotter.py
+++ helpers/plotter.py
@@ -175,7 +175,7 @@ def infer_plot2D(decoder, x, y, em, em_2, num_steps=100, samples=10, order=True,
     axs.plot(x1, y1, c='black')
     for i, inf in enumerate(range(samples)):
         _, _, y_inf, _ = infer.inference(decoder, x=em_infer, y=y_infer, num_steps=num_steps,
-                                      sample=True, d=True, x_2=em2_infer, infer=True, xx = x_infer, yy=yy, x0=x0, y0=y0, x1=x1, y1=y1)
+                                      sample=True, d=True, x_2=em2_infer, infer=True, xx = x_infer.reshape(-1), yy=yy.reshape(-1), x0=x0, y0=y0, x1=x1, y1=y1)
 
         axs.scatter(x_infer.reshape(-1)[s_step:], y_inf.numpy().reshape(-1)[s_step:], c='lightskyblue')
 
