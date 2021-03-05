500a882ec7ee35bee79e496c019cbd23ffc98555
oremnirv <xyfqap-9tydni-voZpig>
2021-03-05 16:43:42 +0000

    fixed an issue with gather in helper

--- helpers/plotter.py
+++ helpers/plotter.py
@@ -165,6 +165,7 @@ def infer_plot2D(decoder, x, y, em, em_2, num_steps=100, samples=10, order=True,
 
     y_infer = np.concatenate((y1, y0[:context_p])).reshape(1, -1)
     s_step = len(y_infer)
+    print('s_step: ', s_step)
     yy = np.concatenate((y1, y0)).reshape(1, -1)
     x_infer = np.concatenate((x1, x0)).reshape(1, -1)
     em_infer = np.concatenate((em1, em0)).reshape(1, -1)
