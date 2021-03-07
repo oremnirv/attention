b7eafb3c832033ec26b30d20a17be3ae8a8135fc
oremnirv <xyfqap-9tydni-voZpig>
2021-03-07 22:58:59 +0000

    In run 6 loss went down to -0.1 and MSE stabilized at 0.46. I Will add now embedding for y and will try to overfit in run 7.

--- data/data_generation.py
+++ data/data_generation.py
@@ -214,8 +214,7 @@ def data_gen2d(num_obs, tr_percent=0.8, seq_len=200, bias='const', kernel='rbf',
         em_y_idx[int(i / 2), :] = idx.idxs[1]
 
         df[i, :x.shape[1]] = x
-        print(x)
-        print(x.shape)
+        print(y.shape)
         print(df.shape)
         df[i + 1, :x.shape[1]] = y.reshape(1, -1)
         for j in range(inp_d):
