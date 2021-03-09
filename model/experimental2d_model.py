db171fa80dba637c229f0051f72ec8387f6453b5
oremnirv <xyfqap-9tydni-voZpig>
2021-03-09 22:26:52 +0000

    Added description to call in exp2D model

--- model/experimental2d_model.py
+++ model/experimental2d_model.py
@@ -23,20 +23,14 @@ class Decoder(tf.keras.Model):
     def call(self, x, x_2, y, training, x_mask, infer=False, ix=None, iy=None, n=0, x0=None, y0=None, x1=None, y1=None):
         """
 
-        :param x:
-        :param x_2:
-        :param y:
+        :param x: (np.array of int) of indices associated with a range of continuous x-vals
+        :param x_2: (np.array) of zeros and ones, identifying from which sequence member (out of a pair) each
+        data point came from.
+        :param y: (np.array float) target variables.
         :param training: (bool) must be TRUE when training and False otherwise
-        :param x_mask:
-        :param infer:
-        :param ix:
-        :param iy:
-        :param n:
-        :param x0:
-        :param y0:
-        :param x1:
-        :param y1:
+        :param x_mask: (np.array) of zeros where a prediction is wanted and 1 otherwise
         :return:
+        2D (tf.tensor) with first dimension being the mean and second the log sigma
         """
         y = y[:, :, tf.newaxis]  # (batch_size, seq_len, 1)
         x = self.embedding(x)  # (batch_size, seq_len + 1, e)
